6469a0dedb4cd7fbade46c6dcc8e264f064e6efa
aldocesarmg <aldocesarmg@gmail.com>
2023-05-25 13:21:56 -0600

    Insert one and Insert many

--- connection.js
+++ connection.js
@@ -5,7 +5,37 @@ const client = new MongoClient(uri);
 async function main() {
     try{
         await client.connect();
-        await listDatabases(client);
+        // await listDatabases(client);
+        await createListing(client, {
+            name: "Lovely Loft",
+            summary: "A charming loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        });
+        await createMultipleListings(client, [
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ]);
     } catch (e) {
         console.error(e);
     } finally {
@@ -13,6 +43,7 @@ async function main() {
     }
 }
 
+// EXAMPLE
 async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
 
@@ -20,6 +51,17 @@ async function listDatabases(client) {
     databasesList.databases.forEach(db => {
         console.log(` - ${db.name}`);
     });
+}
+
+async function createListing(client, newListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function createMultipleListings(client, newListings) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds);
 }
 
 main().catch(console.error);
